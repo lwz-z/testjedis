f9259c4f08b0de61622ba280d8b446f433927006
asus <asus@LAPTOP-2JSKF3JA>
2021-10-11 14:13:02 +0800

    test001

--- testjedis/src/test/java/testjedis/testjedis/test.java
+++ testjedis/src/test/java/testjedis/testjedis/test.java
@@ -5,14 +5,11 @@ import redis.clients.jedis.Jedis;
 public class test {
 
 	public static void main(String[] args) {
-		
+		//TODO Auto-generated method stub
 		Jedis jedis = new Jedis("localhost");
 		jedis.set("foo", "bar");
 		String value = jedis.get("foo");
-
 		System.out.print(value);
 		
-		
 	}
-	
 }
